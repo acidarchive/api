62210e2cf6ce1b96190900bec174985617b40824
db303 <kaukole@protonmail.com>
2024-11-23 22:53:24 +0000

    init: add existing codebase

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use api::configuration::get_configuration;
+use api::startup::Application;
+use api::telemetry::{get_subscriber, init_subscriber};
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    dotenvy::dotenv()?;
+
+    let subscriber = get_subscriber("api".into(), "info".into(), std::io::stdout);
+    init_subscriber(subscriber);
+
+    let configuration = get_configuration().expect("Failed to read configuration.");
+
+    let application = Application::build(configuration).await?;
+    application.run_until_stopped().await?;
+
+    Ok(())
+}
